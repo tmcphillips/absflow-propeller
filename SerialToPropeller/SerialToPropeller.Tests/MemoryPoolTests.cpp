cb891eeb55164b38bd0659f06576e845902982af
Timothy McPhillips <tmcphillips@absoluteflow.org>
2013-09-10 11:14:15 -0700

    Added more tests of MemoryPool and used non-static member initialiation of propeller field.

--- SerialToPropeller/SerialToPropeller.Tests/MemoryPoolTests.cpp
+++ SerialToPropeller/SerialToPropeller.Tests/MemoryPoolTests.cpp
@@ -9,11 +9,9 @@ TEST_CLASS(MemoryPoolTests)
 
 private:
 
-	AbsFlow::Propeller::SerialConnection propeller;
+	Propeller::SerialConnection propeller{ "COM4" };
 
 public:
-
-	MemoryPoolTests() : propeller("COM4") {}
 
 	TEST_METHOD(TestInitializePool)
 	{
@@ -24,13 +22,10 @@ public:
 
 		Propeller::AreEqual(0, propeller << 'L');
 		Propeller::AreEqual(1024, propeller << 'C');
+		Propeller::AreEqual(0, propeller << 'S');
 
-		propeller << 'P';
-		propeller >> poolStartAddress;
-
-		propeller << 'N';
-		propeller >> nextAllocationAddress;
-
+		propeller << 'P', propeller >> poolStartAddress;
+		propeller << 'N', propeller >> nextAllocationAddress;
 		Assert::AreEqual(poolStartAddress, nextAllocationAddress);
 	}
 
@@ -41,17 +36,13 @@ public:
 		int nextAllocationAddress;
 
 		Propeller::AssertTrue(propeller << 'I' << 1024);
+		propeller << 'P', propeller >> poolStartAddress;
 
-		propeller << 'P';
-		propeller >> poolStartAddress;
-
-		propeller << 'A' << 16;
-		propeller >> allocatedMemoryAddress;
-
+		propeller << 'A' << 16, propeller >> allocatedMemoryAddress;
 		Assert::AreEqual(poolStartAddress, allocatedMemoryAddress);
+		Propeller::AreEqual(16, propeller << 'S');
 
-		propeller << 'N';
-		propeller >> nextAllocationAddress;
+		propeller << 'N', propeller >> nextAllocationAddress;
 		Assert::AreEqual(poolStartAddress + 16, nextAllocationAddress);
 	}
 
@@ -62,17 +53,13 @@ public:
 		int nextAllocationAddress;
 
 		Propeller::AssertTrue(propeller << 'I' << 1024);
+		propeller << 'P', propeller >> poolStartAddress;
 
-		propeller << 'P';
-		propeller >> poolStartAddress;
-
-		propeller << 'A' << 15;
-		propeller >> allocatedMemoryAddress;
-
+		propeller << 'A' << 15, propeller >> allocatedMemoryAddress;
+		Propeller::AreEqual(16, propeller << 'S');
 		Assert::AreEqual(poolStartAddress, allocatedMemoryAddress);
 
-		propeller << 'N';
-		propeller >> nextAllocationAddress;
+		propeller << 'N', propeller >> nextAllocationAddress;
 		Assert::AreEqual(poolStartAddress + 16, nextAllocationAddress);
 	}
 
@@ -83,17 +70,12 @@ public:
 		int nextAllocationAddress;
 
 		Propeller::AssertTrue(propeller << 'I' << 1024);
+		propeller << 'P', propeller >> poolStartAddress;
 
-		propeller << 'P';
-		propeller >> poolStartAddress;
-
-		propeller << 'A' << 1;
-		propeller >> allocatedMemoryAddress;
-
+		propeller << 'A' << 1, propeller >> allocatedMemoryAddress;
 		Assert::AreEqual(poolStartAddress, allocatedMemoryAddress);
 
-		propeller << 'N';
-		propeller >> nextAllocationAddress;
+		propeller << 'N', propeller >> nextAllocationAddress;
 		Assert::AreEqual(poolStartAddress + 4, nextAllocationAddress);
 	}
 
@@ -102,23 +84,20 @@ public:
 		int allocationAddress;
 		int expectedNextAllocationAddress;
 		int nextAllocationAddress;
+		int expectedSize = 0;
 		
 		Propeller::AssertTrue(propeller << 'I' << 1024);
+		propeller << 'P', propeller >> expectedNextAllocationAddress;
 
-		propeller << 'P';
-		propeller >> expectedNextAllocationAddress;
+		for (auto request_size_in_bytes : { 4, 12, 256, 32, 96 }) {
 
-		for (auto b : { 4, 12, 256, 32, 96 }) {
-
-			propeller << 'A' << b;
-			propeller >> allocationAddress;
+			propeller << 'A' << request_size_in_bytes, propeller >> allocationAddress;
 			Assert::AreEqual(expectedNextAllocationAddress, allocationAddress);
 
-			expectedNextAllocationAddress += b;
+			Propeller::AreEqual(expectedSize += request_size_in_bytes, propeller << 'S');
 
-			propeller << 'N';
-			propeller >> nextAllocationAddress;
-
+			propeller << 'N', propeller >> nextAllocationAddress;
+			expectedNextAllocationAddress += request_size_in_bytes;
 			Assert::AreEqual(expectedNextAllocationAddress, nextAllocationAddress);
 		}
 	}
@@ -128,6 +107,7 @@ public:
 		int allocationAddress;
 		int expectedNextAllocationAddress;
 		int nextAllocationAddress;
+		int expectedSize = 0;
 
 		Propeller::AssertTrue(propeller << 'I' << 1024);
 
@@ -136,16 +116,65 @@ public:
 
 		for (auto b : { 2, 11, 291, 17, 93 }) {
 
-			propeller << 'A' << b;
-			propeller >> allocationAddress;
+			propeller << 'A' << b, propeller >> allocationAddress;
 			Assert::AreEqual(expectedNextAllocationAddress, allocationAddress);
 
-			expectedNextAllocationAddress += ((b + 3) / 4) * 4;
+			int expectedAllocationBytes = ((b + 3) / 4) * 4;
 
-			propeller << 'N';
-			propeller >> nextAllocationAddress;
+			Propeller::AreEqual(expectedSize += expectedAllocationBytes, propeller << 'S');
 
+			expectedNextAllocationAddress += expectedAllocationBytes;
+
+			propeller << 'N', propeller >> nextAllocationAddress;
 			Assert::AreEqual(expectedNextAllocationAddress, nextAllocationAddress);
 		}
+	}
+
+	TEST_METHOD(TestFirstAllocation_MultipleOfFour_InsufficientMemory)
+	{
+		Propeller::AssertTrue(propeller << 'I' << 8);
+		Propeller::AssertFalse(propeller << 'A' << 16);
+	}
+
+	TEST_METHOD(TestMultipleAllocations_MultiplesOfFour_InsufficientMemory)
+	{
+		Propeller::AssertTrue(propeller << 'I' << 64);
+		Propeller::AreEqual(0, propeller << 'S');
+
+		Propeller::AssertTrue(propeller << 'A' << 16);
+		Propeller::AreEqual(16, propeller << 'S');
+
+		Propeller::AssertTrue(propeller << 'A' << 4);
+		Propeller::AreEqual(20, propeller << 'S');
+
+		Propeller::AssertTrue(propeller << 'A' << 32);
+		Propeller::AreEqual(52, propeller << 'S');
+
+		Propeller::AssertFalse(propeller << 'A' << 16);
+		Propeller::AreEqual(52, propeller << 'S');
+	}
+
+	TEST_METHOD(TestFirstAllocation_NotMultipleOfFour_InsufficientMemory)
+	{
+		Propeller::AssertTrue(propeller << 'I' << 8);
+		Propeller::AssertFalse(propeller << 'A' << 16);
+	}
+
+	TEST_METHOD(TestMultipleAllocations_NotMultiplesOfFour_InsufficientMemory)
+	{
+		Propeller::AssertTrue(propeller << 'I' << 64);
+		Propeller::AreEqual(0, propeller << 'S');
+
+		Propeller::AssertTrue(propeller << 'A' << 7);
+		Propeller::AreEqual(8, propeller << 'S');
+
+		Propeller::AssertTrue(propeller << 'A' << 33);
+		Propeller::AreEqual(44, propeller << 'S');
+
+		Propeller::AssertTrue(propeller << 'A' << 10);
+		Propeller::AreEqual(56, propeller << 'S');
+
+		Propeller::AssertFalse(propeller << 'A' << 16);
+		Propeller::AreEqual(56, propeller << 'S');
 	}
 };
